126d4e88d3d17d63082b685dc5c219088b3f0fe0
ikh831 <irene.haque@gmail.com>
2013-05-01 17:33:03 -0700

    mobile accordion and refactoring

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 //= require_tree .
+//
+//
+
+$(document).ready(function() {
+  ["schedule","sponsors","register","location"].map( function(id) {
+    var element = "h1#" + id + ".mobile";
+    $(element).click(function () {
+      if ($(element).next().is(":hidden")) {
+        $(element).next().show('fast');
+      } else {
+        $(element).next().slideUp();
+      }
+    });
+  });
+
+});
+
